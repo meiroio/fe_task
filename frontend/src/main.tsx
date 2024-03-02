4b239b9a6685190c3db973c16bea176573310bcb
Martin Began <martin.began@kiwi.com>
2024-03-02 14:16:56 +0100

    feat: adds tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 
+import './index.css';
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
